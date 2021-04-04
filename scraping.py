e149a7c67f45f71223aec0bfe0a9d20aa2d118ad
Allan Cordeiro <allan.cordeiro.santos@gmail.com>
2021-04-04 19:50:56 -0300

    classe scraping e regras de controle para parar a consulta.

---
+++ scraping.py
@@ -0,0 +1,56 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import UnexpectedAlertPresentException
+import time
+import logging
+
+class Scraping:
+    def __init__(self, uri, award_number=""):
+        self._uri = uri
+        self._award_number = award_number
+        self._data = []
+        self._control_number = 0
+        logging.basicConfig(filename='logs/scrapping.log', level=logging.INFO)
+
+    def scrap_award(self):
+        with webdriver.Firefox(executable_path=r'geckodriver/geckodriver') as driver:
+            wait = WebDriverWait(driver, 10)
+            driver.get(self._uri)
+            driver.find_element(By.ID, 'buscaConcurso').send_keys(self._award_number + Keys.ENTER)
+            while True:
+                try:
+                    time.sleep(2)
+                    award_data = driver.find_element_by_class_name("title-bar.clearfix").\
+                        find_element_by_tag_name("span").text
+                    print(f'Raspando dados de: {award_data}')
+                    lucky_numbers = driver.find_element_by_id("ulDezenas").find_elements_by_tag_name("li")
+                    winners_data = driver.find_element_by_class_name("related-box.gray-text.no-margin").text
+                    forecast_data = driver.find_element_by_class_name("totals").text
+                    consolidated = [award_data, lucky_numbers, winners_data, forecast_data]
+                    self._data.append(consolidated)
+                    current_award = self._loop_control(award_data)
+                    print(f"controle {self._control_number} | atual {current_award}")
+                    if current_award != self._control_number:
+                        self._control_number = current_award
+                    else:
+                        logging.info(f'{time.strftime("%m/%d/%Y %I:%M:%S %p")}: Fim da execução')
+                        break
+                    driver.find_element_by_css_selector("a[ng-click='carregaProximoConcurso()']").click()
+                    time.sleep(4)
+                except UnexpectedAlertPresentException as AlertE:
+                    logging.info(f'{time.strftime("%m/%d/%Y %I:%M:%S %p")}: Chegamos no fim da página')
+                    logging.error(f'{AlertE.msg} | {AlertE.alert_text}')
+                    break
+                except Exception as e:
+                    logging.error(f'{time.strftime("%m/%d/%Y %I:%M:%S %p")}: Ocorreu um erro não esperado')
+                    logging.error(f'-->{e.args}')
+                    logging.error(f'--> {award_data}')
+                    break
+        return self._data
+
+    @staticmethod
+    def _loop_control(award_data):
+        info = award_data.split(" ")
+        return int(info[1])
