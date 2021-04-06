2d16a89d1ed441746d8c9067a4b56d47c47a74d7
Allan Cordeiro <allan.cordeiro.santos@gmail.com>
2021-04-05 21:54:42 -0300

    Adicionado controle diferencial da carga de dados.

--- main.py
+++ main.py
@@ -3,10 +3,21 @@ from scraping import Scraping
 from normalization import Normalize
 from conf import Config
 
+
+def get_data():
+    empty_data = []
+    try:
+        with open('data/megasena.json', 'r') as outfile:
+            data = json.load(outfile)
+            return data
+    except FileNotFoundError:
+        return empty_data
+
+
 def main():
     uri = Config.get_config('uri')
     prize = Config.get_config('last_prize_scraped')
-    final_json = []
+    final_json = get_data()
     scraping = Scraping(
         uri,
         prize
@@ -18,6 +29,7 @@ def main():
 
     with open('data/megasena.json', 'w') as outfile:
         json.dump(final_json, outfile)
+
 
 if __name__ == '__main__':
     main()
